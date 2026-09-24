a9153621484a52aff79836d33ac878131fd15488
Edgar Mesquita <edgar@equantic.tech>
2026-09-24 12:37:00 +0100

    🐛 fix: char.IsWhiteSpace reads .NET's white space
    .NET's white space is the Unicode White_Space property, and JavaScript's
    \s is not: it leaves out NEXT LINE (U+0085) and takes in the byte order
    mark (U+FEFF). Measured over the whole BMP on both sides, those two are
    the only difference. The test is \p{White_Space} now, for the char
    overload and the (string, index) one, and the code editor's transpiled
    twins, which call it seventeen times, are regenerated.

--- src/eQuantic.UI.Runtime/src/shared/components/CodeDocument.ts
+++ src/eQuantic.UI.Runtime/src/shared/components/CodeDocument.ts
@@ -144,9 +144,9 @@ export class CodeDocument {
         if (!CodeDocument.isWordChar(line[index])) {
             if (here.column > 0 && CodeDocument.isWordChar(line[here.column - 1])) index = here.column - 1; else {
                 let symbolStart = index;
-                while (symbolStart > 0 && !CodeDocument.isWordChar(line[symbolStart - 1]) && !(/^\s$/.test(line[symbolStart - 1]))) symbolStart--;
+                while (symbolStart > 0 && !CodeDocument.isWordChar(line[symbolStart - 1]) && !(/^\p{White_Space}$/u.test(line[symbolStart - 1]))) symbolStart--;
                 let symbolEnd = index;
-                while (symbolEnd < line.length && !CodeDocument.isWordChar(line[symbolEnd]) && !(/^\s$/.test(line[symbolEnd]))) symbolEnd++;
+                while (symbolEnd < line.length && !CodeDocument.isWordChar(line[symbolEnd]) && !(/^\p{White_Space}$/u.test(line[symbolEnd]))) symbolEnd++;
                 return new CodeRange($eq.withPatch(here, { column: symbolStart }), $eq.withPatch(here, { column: symbolEnd }));
             }
         }
